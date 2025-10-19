cf3f6195d80ec75547d00fc709369c796638f1c9
Peter Suggitt <me@suggs.org.uk>
2025-10-19 17:48:30 +0100

    updated dir

--- setupenv.sh
+++ setupenv.sh
@@ -15,7 +15,7 @@ rm -fr apache-maven-*
 
 echo "Running in ${SCRIPT_DIR}"
 echo "📦 Downloading Apache Maven ${MVN_VERSION}..."
-if curl -o ${MVN_FILE} -sfLO "$MVN_URL"; then
+if curl -o ${MVN_FILE} -fLO "$MVN_URL"; then
     echo "✅ Download complete."
 else
     echo "❌ Download failed from $MVN_URL"
@@ -26,7 +26,7 @@ tar -xf ${MVN_FILE}
 rm ${MVN_FILE}
 
 echo "📦 Downloading Zulu 11 from ${ZULU11_URL}"
-if curl -o ${ZULU11_FILE} -sfLO "$ZULU11_URL"; then
+if curl -o ${ZULU11_FILE} -fLO "$ZULU11_URL"; then
     echo "✅ Download complete."
 else
     echo "❌ Download failed from $ZULU11_URL"
@@ -34,9 +34,14 @@ else
 fi
 
 echo "📦 Downloading Zulu 21 from ${ZULU21_URL}"
-if curl -o ${ZULU21_FILE} -sfLO "$ZULU21_URL"; then
+if curl -o ${ZULU21_FILE} -fLO "$ZULU21_URL"; then
     echo "✅ Download complete."
 else
     echo "❌ Download failed from $ZULU21_URL"
     exit 1
 fi
+
+tar -xf ${ZULU11_FILE}
+tar -xf ${ZULU21_FILE}
+rm ${ZULU11_FILE}
+rm ${ZULU21_FILE}
